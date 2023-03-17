00a706ab4a8c595ebc1fb3ea86dfabf4ef6f8324
AdamBenDev <adamaya9732@gmail.com>
2023-03-17 15:54:42 +0100

    html levels

--- main.js
+++ main.js
@@ -3,28 +3,23 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.*/
 
-//*Seleziono il contenitore della griglia
-
-const gridContainer = document.getElementById("grid")
 
 
-//*Creo una matrice 10x10 con numeri progressivi 
+//* Otteniamo gli elementi HTML che useremo
 
-const gridValues = [];
-let counter = 1;
-for (let index = 0; index < 10; index++) {
-    const row = [];
-    for (let j=0; j < 10; j++) {
-        row.push(counter++);
-    }
-    gridValues.push(row)
-    
+const generateGridButton = document.getElementById("generate-grid-button");
+const difficultySelect = document.getElementById("difficulty-select");
+const gridContainer = document.getElementById("grid-container");
+
+//* Aggiungiamo un evento click al bottone "Genera griglia"
+
+
+generateGridButton.addEventListener("click", function() {
+
+    //* Leggiamo il livello di difficoltà scelto dall'utente
+
+    const difficultyLevel = parseInt(difficultySelect.value);
+
 }
+  
 
-//*creo gli elementi in html per la griglia
-
-const gridElements = [];
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    
-}
